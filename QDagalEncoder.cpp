3db887bb007923e3839cc56547f0b4819b24f5a8
Dagal <dejarding@gmail.com>
2014-09-25 23:26:27 +0200

    Création de l'encodeur

--- QDagalEncoder.cpp
+++ QDagalEncoder.cpp
@@ -1,6 +1,50 @@
-#include "QDagalEncoder.h"
+﻿#include "QDagalEncoder.h"
+
+#include <math.h>
 
 QDagalEncoder::QDagalEncoder(QWidget *parent) :
-	QWidget(parent)
+	QAbstractSlider(parent)
 {
+	setValue(0);
+	setMinimum(0);
+	setMaximum(127);
+	setMinimumWidth(60);
+	setMinimumHeight(60);
+}
+
+void QDagalEncoder::paintEvent(QPaintEvent *event)
+{
+	Q_UNUSED(event);
+
+	QPainter painter(this);
+	QPen pen;
+	pen.setColor(Qt::blue);
+	pen.setWidth(1);
+	painter.setPen(pen);
+	QBrush brush(Qt::SolidPattern);
+	brush.setColor(Qt::white);
+	painter.setBrush(brush);
+	painter.drawEllipse(5,5,50,50);
+	QPen pen2;
+	pen2.setColor(Qt::blue);
+	pen2.setWidth(3);
+	painter.setPen(pen2);
+	painter.drawEllipse(20,20,20,20);
+	painter.setPen(pen);
+	float angle = -(value()-minimum())/(maximum()-minimum())*7.0/M_PI_4 - M_PI_4/2.0;
+	float x;
+	for (x = 0.0; x < 2 * M_PI; x += M_PI_4 / 2.0)
+	{
+		painter.drawLine(30.5+((x==0.0)?0:(10*sin(x+angle))), 30.5+((x==0.0)?0:(10*cos(x+angle))),
+										 30.5+20*sin(x+angle),30.5+20*cos(x+angle));
+	}
+}
+
+void QDagalEncoder::setValue(int value)
+{
+	if (value != QAbstractSlider::value())
+	{
+		QAbstractSlider::setValue(value);
+		update();
+	}
 }
--- QDagalEncoder.h
+++ QDagalEncoder.h
@@ -1,14 +1,24 @@
-#ifndef QDAGALENCODER_H
+﻿#ifndef QDAGALENCODER_H
 #define QDAGALENCODER_H
 
-#include <QWidget>
+#include <QAbstractSlider>
+#include <QPainter>
 
-class QDagalEncoder : public QWidget
+class QDagalEncoder : public QAbstractSlider
 {
 	Q_OBJECT
 
 public:
 	QDagalEncoder(QWidget *parent = 0);
+
+	void paintEvent(QPaintEvent *event);
+
+public slots:
+	void setValue(int value);
+
+signals:
+
+private:
 };
 
 #endif
