385af7435531db118b1b8c3ec17769e74768e97c
Dagal <dejarding@gmail.com>
2014-09-29 23:12:05 +0200

    Création du switch en cours!

--- QDagalSwitch.cpp
+++ QDagalSwitch.cpp
@@ -1,6 +1,60 @@
-#include "QDagalSwitch.h"
+﻿#include "QDagalSwitch.h"
 
 QDagalSwitch::QDagalSwitch(QWidget *parent) :
-	QWidget(parent)
+	QWidget(parent),
+	m_value(0)
 {
+	setMinimumWidth(26);
+	setMinimumHeight(46);
 }
+
+void QDagalSwitch::paintEvent(QPaintEvent* event)
+{
+	Q_UNUSED(event);
+
+	int centerX = width() / 2;
+	int centerY = height() / 2;
+	int largeur = (width() < centerY)?(width() - 6):((height() - 6) / 2);
+	int hauteur = largeur * 2;
+
+	QPainter painter(this);
+	QPen pen;
+	pen.setColor(Qt::black);
+	pen.setWidth(1);
+	painter.setPen(pen);
+	QBrush brush(Qt::SolidPattern);
+	brush.setColor(Qt::gray);
+	painter.setBrush(brush);
+	painter.drawRoundedRect(centerX - largeur / 2,
+													centerY - hauteur / 2,
+													largeur,
+													hauteur,
+													largeur / 2,
+													largeur / 2);
+
+	painter.drawEllipse(centerX - largeur * 0.4,
+											centerY + largeur * (m_value == 0)?0.1:-0.9,
+											largeur * 0.8,
+											largeur * 0.8);
+}
+
+int QDagalSwitch::value() const
+{
+	return m_value;
+}
+
+void QDagalSwitch::setValue(int value)
+{
+	int tmp = value;
+	if (tmp < 64)
+		tmp = 0;
+	else
+		tmp = 127;
+	if (tmp != m_value)
+	{
+		m_value = tmp;
+		emit(valueChanged(tmp));
+		update();
+	}
+}
+
--- QDagalSwitch.h
+++ QDagalSwitch.h
@@ -1,14 +1,29 @@
-#ifndef QDAGALSWITCH_H
+﻿#ifndef QDAGALSWITCH_H
 #define QDAGALSWITCH_H
 
 #include <QWidget>
+#include <QPainter>
 
 class QDagalSwitch : public QWidget
 {
 	Q_OBJECT
 
+	Q_PROPERTY(int value READ value WRITE setValue NOTIFY valueChanged)
+
 public:
 	QDagalSwitch(QWidget *parent = 0);
+
+	void paintEvent(QPaintEvent* event);
+	int value() const;
+
+public slots:
+	void setValue(int value);
+
+signals:
+	void valueChanged(int value);
+
+private:
+	int m_value;
 };
 
 #endif
